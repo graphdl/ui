55789ce306030984ee3785cb276c2674011b833e
nathanclevenger <4130910+nathanclevenger@users.noreply.github.com>
2023-02-26 15:09:31 -0600

    A few more tweaks

--- packages/ui/layouts/Admin.js
+++ packages/ui/layouts/Admin.js
@@ -1,10 +1,13 @@
 import { Shell } from './Shell'
-import { CodeList } from '../lists/CodeList'
+import { TableList } from '../lists/TableList'
+import { CardList } from '../lists/CardList'
 
 export const Admin = ({ graph, params, view, children }) => (
   <Shell params={params} view={view}>
-    {view === 'create' && <h1>Create {params.noun}</h1>}
-    {CodeList}
+    {
+     view == 'list' && <TableList/> ||
+     view == 'create' && <h1>Detail {params?.noun}</h1> ||
+     <h1>Create {params?.noun}</h1>} 
   </Shell>
 )
 
--- packages/ui/lists/CardList.js
+++ packages/ui/lists/CardList.js
@@ -8,7 +8,7 @@ const defaultItems = [
   },
 ]
 
-export const CardList = (items = defaultItems) => {
+export const CardList = ({items = defaultItems}) => {
   return (
     <div className="grid grid-cols-1 gap-4 sm:grid-cols-2">
       {items.map((item) => (
